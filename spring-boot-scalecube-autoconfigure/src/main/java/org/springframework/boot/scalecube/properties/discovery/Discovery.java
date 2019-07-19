1d2849a40c9004af4f2463969859af6c999fb123
eutkin <eutkin@custis.ru>
2019-07-19 16:39:07 +0300

    Remove metadata & network type configuration parameters

--- spring-boot-scalecube-autoconfigure/src/main/java/org/springframework/boot/scalecube/properties/discovery/Discovery.java
+++ spring-boot-scalecube-autoconfigure/src/main/java/org/springframework/boot/scalecube/properties/discovery/Discovery.java
@@ -3,16 +3,8 @@ package org.springframework.boot.scalecube.properties.discovery;
 import static org.springframework.boot.scalecube.properties.ScalecubeProperties.setProperty;
 
 import io.scalecube.cluster.ClusterConfig;
-import io.scalecube.cluster.metadata.MetadataDecoder;
-import io.scalecube.cluster.metadata.MetadataEncoder;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.UnaryOperator;
-import lombok.SneakyThrows;
 
 /**
  * Properties for configuration discovery service.
@@ -23,60 +15,20 @@ public class Discovery implements UnaryOperator<ClusterConfig> {
   private DiscoveryMembership membership = new DiscoveryMembership();
   private DiscoveryGossip gossip = new DiscoveryGossip();
   private DiscoveryFailureDetector failureDetector = new DiscoveryFailureDetector();
-  private DiscoveryTransportType type = DiscoveryTransportType.LAN;
-
-  private Map<String, String> metadata = new HashMap<>();
 
   private String memberHost;
   private Integer memberPort;
 
 
-  // fixme metadata and type network
   @Override
   public ClusterConfig apply(ClusterConfig clusterConfig) {
-    // FIXME: 16.07.2019 https://github.com/scalecube/scalecube-services/issues/631
     ClusterConfig tempConfig = clusterConfig;
-//    switch (getType()) {
-//      case WAN:
-//        tempConfig = ClusterConfig
-//            .defaultWanConfig();
-//        break;
-//      case LAN:
-//        tempConfig = ClusterConfig.defaultLanConfig();
-//        break;
-//      case LOCAL:
-//        tempConfig = ClusterConfig.defaultLocalConfig();
-//        break;
-//      default:
-//        tempConfig = ClusterConfig.defaultConfig();
-//    }
     tempConfig = tempConfig.transport(getTransport());
     tempConfig = tempConfig.membership(getMembership());
     tempConfig = tempConfig.gossip(getGossip());
     tempConfig = tempConfig.failureDetector(getFailureDetector());
     tempConfig = setProperty(tempConfig, tempConfig::memberHost, this::getMemberHost);
     tempConfig = setProperty(tempConfig, tempConfig::memberPort, this::getMemberPort);
-    metadata.put("unic", UUID.randomUUID().toString());
-    tempConfig = tempConfig.metadata(getMetadata());
-    tempConfig = tempConfig.metadataDecoder(new MetadataDecoder() {
-
-
-      @SneakyThrows
-      @Override
-      public Object decode(ByteBuffer buffer) {
-        return Collections.singletonMap("unic", UUID.randomUUID().toString());
-      }
-    });
-
-    tempConfig = tempConfig.metadataEncoder(new MetadataEncoder() {
-      @SneakyThrows
-      @Override
-      public ByteBuffer encode(Object metadata) {
-        Map<String, String> map = (Map<String, String>) metadata;
-        String unic = map.get("unic");
-        return ByteBuffer.wrap(("{\"unic\" : \"" + unic + "\"}").getBytes());
-      }
-    });
     return tempConfig;
   }
 
@@ -113,14 +65,6 @@ public class Discovery implements UnaryOperator<ClusterConfig> {
     this.failureDetector = failureDetector;
   }
 
-  public DiscoveryTransportType getType() {
-    return type;
-  }
-
-  public void setType(DiscoveryTransportType type) {
-    this.type = type;
-  }
-
   public Optional<String> getMemberHost() {
     return Optional.ofNullable(memberHost);
   }
@@ -135,13 +79,5 @@ public class Discovery implements UnaryOperator<ClusterConfig> {
 
   public void setMemberPort(Integer memberPort) {
     this.memberPort = memberPort;
-  }
-
-  public Map<String, String> getMetadata() {
-    return metadata;
-  }
-
-  public void setMetadata(Map<String, String> metadata) {
-    this.metadata = metadata;
   }
 }

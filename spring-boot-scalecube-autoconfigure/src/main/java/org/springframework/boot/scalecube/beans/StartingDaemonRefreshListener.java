89e0441e39a8fa1d9c5650229f065c96cbd3adef
eutkin <eutkin@custis.ru>
2019-07-22 12:14:12 +0300

    Await thread not started if app is web app (app have await thread already)

--- spring-boot-scalecube-autoconfigure/src/main/java/org/springframework/boot/scalecube/beans/StartingDaemonRefreshListener.java
+++ spring-boot-scalecube-autoconfigure/src/main/java/org/springframework/boot/scalecube/beans/StartingDaemonRefreshListener.java
@@ -2,17 +2,30 @@ package org.springframework.boot.scalecube.beans;
 
 import io.scalecube.services.Microservices;
 import org.springframework.beans.factory.BeanClassLoaderAware;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.util.ClassUtils;
 
 public class StartingDaemonRefreshListener implements ApplicationListener<ContextRefreshedEvent>,
-    BeanClassLoaderAware, DisposableBean {
+    BeanClassLoaderAware {
+
+  private static final String SERVLET_APPLICATION_CONTEXT_CLASS =
+      "org.springframework.web.context.WebApplicationContext";
+
+  private static final String REACTIVE_APPLICATION_CONTEXT_CLASS =
+      "org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext";
 
   private ClassLoader classLoader;
 
   @Override
   public void onApplicationEvent(ContextRefreshedEvent event) {
+    Class<?> applicationContextClass = event.getApplicationContext().getClass();
+    boolean webApp =
+        isAssignable(SERVLET_APPLICATION_CONTEXT_CLASS, applicationContextClass) || isAssignable(
+            REACTIVE_APPLICATION_CONTEXT_CLASS, applicationContextClass);
+    if (webApp) {
+      return;
+    }
     Thread awaitThread = new Thread(() -> {
       Microservices microservices = event.getApplicationContext().getBean(Microservices.class);
       microservices.onShutdown().block();
@@ -28,8 +41,13 @@ public class StartingDaemonRefreshListener implements ApplicationListener<Contex
     this.classLoader = classLoader;
   }
 
-  @Override
-  public void destroy() throws Exception {
-
+  private static boolean isAssignable(String target, Class<?> type) {
+    try {
+      return ClassUtils.resolveClassName(target, null).isAssignableFrom(type);
+    }
+    catch (Throwable ex) {
+      return false;
+    }
   }
+
 }

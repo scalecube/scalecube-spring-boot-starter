ecba7820b6882e3a9c1173d9b055d3f396e502b6
eutkin <utkinev2014@gmail.com>
2020-01-08 19:16:34 +0300

    renaming modules

---
+++ scalecube-spring-boot-examples/rating/src/test/java/io/scalecube/spring/example/rating/RatingApplicationTests.java
@@ -0,0 +1,18 @@
+package io.scalecube.spring.example.rating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RatingApplicationTests {
+
+  @Test
+  public void contextLoads() throws IOException {
+  }
+
+}
